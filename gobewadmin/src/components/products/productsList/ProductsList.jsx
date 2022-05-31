43fdee97094164c23504d88065603ae9ba46f766
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-05-31 11:40:08 -0300

    Listado de productos disponibles ya listo

---
+++ gobewadmin/src/components/products/productsList/ProductsList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import ItemProduct from './ItemProduct';
+
+
+export default function ProductsList() {
+    return (
+        <section>
+            <ul>
+                <ItemProduct/>
+            </ul>
+        </section>
+    )
+}

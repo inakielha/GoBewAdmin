844a7ee205a655c144f7cd66533146d66e559796
genafalconi <genaro.f@hotmail.com>
2022-05-31 12:14:24 -0300

    Creacion form

--- gobewadmin/src/App.js
+++ gobewadmin/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ProductsList from './components/products/productsList/ProductsList';
 import Nav from './components/nav/Nav';
+import CreationForm from './components/products/createForm/CreationForm';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <ProductsList/>
+      <CreationForm/>
     </div>
   );
 }
--- gobewadmin/src/redux/reducer.js
+++ gobewadmin/src/redux/reducer.js
@@ -1,5 +1,5 @@
 import { createReducer } from "@reduxjs/toolkit";
-import { GET_PRODUCTS } from "./actions";
+import { GET_PRODUCTS, CREATE_PRODUCT, CREATE_CATEGORY, GET_CATEGORIES_ADMIN } from "./actions";
 
 
 const initialState = {
@@ -11,5 +11,14 @@ const initialState = {
 export const adminReducer = createReducer(initialState, (builder) => {
     builder.addCase(GET_PRODUCTS.fulfilled, (state, action) => {
         state.products = action.payload.productList;
+    })
+    builder.addCase(CREATE_PRODUCT.fulfilled, (state, action) => {
+        state.product = action.payload
+    })
+    builder.addCase(CREATE_CATEGORY, (state, action) => {
+        state.categories = action.payload.data
+    })
+    builder.addCase(GET_CATEGORIES_ADMIN.fulfilled, (state, action) => {
+        state.categories = action.payload
     })
 })

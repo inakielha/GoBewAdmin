142f00b8587e88b83eb02de3ac6b8f44a788418d
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-05-31 14:25:53 -0300

    Tabla con productos

--- gobewadmin/src/components/products/productsList/ItemProduct.jsx
+++ gobewadmin/src/components/products/productsList/ItemProduct.jsx
@@ -1,29 +1,38 @@
 import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { GET_PRODUCTS } from '../../../redux/actions';
+import styles from '../../styles/tableProducts.module.css'
+
 
 export default function ItemProduct() {
-    
-    const {products} = useSelector((store) => store.adminReducer);
+
+    const { products } = useSelector((store) => store.adminReducer);
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(GET_PRODUCTS())
     }, [dispatch])
-    
+
     return (
-        <>
-            {
-                products?.map(p => {
-                    return (
-                        <li>
-                            <span>{p.productName}</span>
-                            <span>{p.productStock}</span>
-                            <span>{p.productPrice}</span>
-                        </li>
+        <table className={styles.tableContainer}>
+            <thead className={styles.headTable}>
+                <th>Nombre del producto</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+            </thead>
+            <tbody className={styles.bodyTable}>
+                {
+                    products?.map(p => {
+                        return (
+                            <tr>
+                                <td>{p.productName}</td>
+                                <td className={styles.stockContainer}>{p.productStock}</td>
+                                <td className={styles.priceContainer}>{p.productPrice}</td>
+                            </tr>
                         )
-                })
-            }
-        </>
+                    })
+                }
+            </tbody>
+        </table>
     )
 }

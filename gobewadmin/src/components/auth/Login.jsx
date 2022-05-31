5d27c1c7d3458f0441be5a88aa62f368e5d28f48
Soledad Marini <msmarinip@gmail.com>
2022-05-31 17:46:35 -0300

    feat: component TextInput con formik

--- gobewadmin/src/components/auth/Login.jsx
+++ gobewadmin/src/components/auth/Login.jsx
@@ -1,6 +1,20 @@
+import  { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export const Login = () => {
+
+  const { handleSubmit } = useFormik({
+    initialValues: {
+      userEmail:'',
+      userPassword: ''
+    },
+    onSubmit: (values) => console.log(JSON.stringify(values, null, 2))
+
+  });
   return (
-    <div>Login</div>
+    <div>
+      <div>Login</div>
+
+    </div>
   )
 }

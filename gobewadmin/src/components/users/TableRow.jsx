a3bd0b6b8ccbdacec2a7a3f70ce3dd2fb8695425
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-06-07 09:38:18 -0300

    ahora en la tabla de usuarios solo se pueden editar las prioridades (el nombre y el apellido no se editan)

--- gobewadmin/src/components/users/TableRow.jsx
+++ gobewadmin/src/components/users/TableRow.jsx
@@ -52,12 +52,14 @@ export default function TableRow({ userFirstName, userLastName, userEmail, userI
 
     return (
         <tr>
-            {
+            {/* {
                 <td><input onChange={handleChange}  type="text" value={userChange.userFirstName} name="userFirstName" disabled={!edit} /></td>
             }
             {
                 <td><input onChange={handleChange}  type="text" value={userChange.userLastName} name="userLastName" disabled={!edit} /></td>
-            }
+            } */}
+            <td className='field--name'>{userChange.userFirstName}</td>
+            <td className='field--lastName'>{userChange.userLastName}</td>
             <td className='field--email'>{userChange.userEmail}</td>
             <td className='field--active'><input type="checkbox" checked={userChange.userIsActive} name="userIsActive" disabled/></td>
             <td className='field--admin'><input type="checkbox" checked={userChange.userIsAdmin} name="userIsAdmin" onClick={handleCheckEvent} disabled={!edit}/></td>
